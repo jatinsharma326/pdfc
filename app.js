d559364d0d8e48b3e21f573f481da76003fe784b
jatinsharma326 <jatins49@gmail.com>
2021-11-11 15:46:31 +0530

    add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 app = express();
 const path = require("path");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const router = require("./router/index");
 const fs = require("fs");
 let list = "";
--- controllers/index.js
+++ controllers/index.js
@@ -72,9 +72,6 @@ const leftrightContent = [
 ];
 
 module.exports.home = (req, res) => {
-  let result = "App is running";
-  res.send(result);
-
   return res.render("index", {
     title: "smallPdf",
     cardContents: cardContent,
